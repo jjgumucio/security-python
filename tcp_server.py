71e96bfe6eb4ea992bd26a6106f8fd15d2c9688f
Joaquín Gumucio <jjgumucio@gmail.com>
2018-11-04 01:13:50 -0300

    Updates to python 3

--- tcp_server.py
+++ tcp_server.py
@@ -1,44 +1,47 @@
+#!/usr/bin/env python3
+
 import socket
 import threading
 import argparse
 
-def serveClient(connection, clientAddress, port):
+def serve_client(connection, client_address, port):
   try:
-    request = connection.recv(4096)
-    print "[!] Received data from client (%s:%d): %s" % (clientAddress, port, request)
+    request = connection.recv(1024)
+    print("[!] Received data from client (%s:%d): %s" % (client_address, port, request))
 
     connection.send("Response from the basic TCP server!")
 
     connection.close()
 
   except Exception as e:
-    print e
+    print("[E] Error serving client with address %s on port %s" % (client_address, port))
 
-def startServer(port):
-  try:
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(("0.0.0.0", port))
-    server.listen(5)
+def start_server(port):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+            server.bind(("0.0.0.0", port))
+            server.listen(5)
 
-    print "[!] Localhost listening on port %d" % port
+            print("[!] Localhost listening on port %d" % port)
 
-    while True:
-      connection, address = server.accept()
-      print "[!] Received connection from %s:%d" % (address[0], int(address[1]))
+            while True:
+                connection, address = server.accept()
+                print("[!] Received connection from %s:%d" % (address[0], int(address[1])))
 
-      serviceThread = threading.Thread(target=serveClient, args=(connection, address[0], address[1]))
-      serviceThread.start()
+            # TODO: Update to use asyncio for concurrency instead of threading
+            service_thread = threading.Thread(target=serve_client, args=(connection, address[0], address[1]))
+            service_thread.start()
 
-  except Exception as e:
-      print "[E] Error on server: %s" % e
+    except Exception as e:
+        print("[E] Error on server: %s" % e)
 
 def main():
-  parser = argparse.ArgumentParser("Basic TCP server")
-  parser.add_argument("-p", "--port", type=int, help="Port number to listen on")
-  args = parser.parse_args()
-  port = args.port
+    parser = argparse.ArgumentParser("Basic TCP server")
+    parser.add_argument("-p", "--port", type=int, help="Port number to listen to")
+    args = parser.parse_args()
+    port = args.port
 
-  startServer(int(port))
+    start_server(int(port))
 
 if __name__ == "__main__":
-  main()
+    main()
